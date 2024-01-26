a0dc897fcd7e7d427c685a9f78727120efc7118d
Logan Markley <loganmarkley03@gmail.com>
2024-01-26 10:35:56 -0600

    more done

--- showcase.js
+++ showcase.js
@@ -54,7 +54,7 @@ function checkInputName() {
         containerElement.style.width = '40%';
         containerElement.style.backgroundColor = 'rgb(226, 71, 103)';
         setTimeout(() => {
-            containerElement.style.animation = 'rockAnimation 1.5s ease-in-out 4';
+            containerElement.style.animation = 'bounceAndRockAnimation 1.5s ease-in-out 4';
         }, 1000);
         
         const labelElement = document.querySelector("body div label");
@@ -63,10 +63,13 @@ function checkInputName() {
         labelElement.innerText = "My girl is here!";
         
         setTimeout(() => {  //after the rocking ends, send her on to the next phase!
-            containerElement.style.transition = 'opacity 3000ms ease-in-out';
+            containerElement.style.transition = 'opacity 2000ms ease-in-out';
             containerElement.style.opacity = '0';
-            phaseNumber1();
-        }, 3000);
+            setTimeout(() => {
+                containerElement.remove();
+                phaseNumber1();
+            }, 3000);
+        }, 4000);
         
     } else {
         const labelElement = document.querySelector("body div label");
@@ -78,7 +81,48 @@ function checkInputName() {
 }
 
 function phaseNumber1() {
-    console.log("phase1");
+    const containerElement = document.createElement("div");
+    const topContainerElement = document.createElement("div");
+    const middleContainerElement = document.createElement("div");
+    const bottomContainerElement = document.createElement("div");
+
+    containerElement.id = "container";
+    topContainerElement.id = "top";
+    middleContainerElement.id = "middle";
+    bottomContainerElement.id = "bottom";
+
+    containerElement.className = "container";
+    topContainerElement.className = "container ilyContainer ";
+    middleContainerElement.className = "container ilyContainer ";
+    bottomContainerElement.className = "container ilyContainer ";
+
+    containerElement.style.display = "flex";
+    containerElement.style.flexDirection = "column";
+    containerElement.style.alignItems = "center";
+    containerElement.style.justifyContent = "space-evenly";
+    containerElement.style.height = "90vh";
+    containerElement.style.removeProperty("box-shadow");
+    containerElement.style.backgroundColor = "transparent";
+
+    topContainerElement.innerText = "I";
+    middleContainerElement.innerText = "love";
+    bottomContainerElement.innerText = "you <3";
+
+    containerElement.style.opacity = "1";
+    setTimeout(() => {
+        topContainerElement.style.opacity = "1";
+        setTimeout(() => {
+            middleContainerElement.style.opacity = "1";
+            setTimeout(() => {
+                bottomContainerElement.style.opacity = "1";
+            }, 750);
+        }, 750);
+    }, 1000);
+
+    containerElement.appendChild(topContainerElement);   
+    containerElement.appendChild(middleContainerElement);   
+    containerElement.appendChild(bottomContainerElement);
+    document.body.appendChild(containerElement);
 }
 function createPuzzle() {
     // const leftSemiCircle = document.createElement('div');
